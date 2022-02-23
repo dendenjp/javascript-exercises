61bc5945f78ed968edbeea539d43d033aa3c045b
dendenjp <daimlerjp@gmail.com>
2022-02-23 08:11:53 -0600

    added solution to 07 tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function (fnum) {
+  // C = F - 32 * (5/9)
+  let celsius = (fnum - 32) * (5 / 9);
+  let roundedCelsius = Math.round(celsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const ctof = function() {
-
+const ctof = function (cnum) {
+  // F = (C * 1.8) + 32
+  let fahrenheit = cnum * 1.8 + 32;
+  let roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
