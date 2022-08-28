2e82a9fba06f08bc43216c10313d4da7029ccc55
dendenjp <daimlerjp@gmail.com>
2022-08-28 02:34:30 -0500

    added solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    // converting to lowercase and removing spaces
+    let myString = string.toLowerCase().split(' ').join('');
 
+    // removing punctuation
+    let myArr = Array.from(myString).filter((punc) => {
+        return punctuation.indexOf(punc) === -1;
+    });
+    //converting array to string(this will be the string input)
+    let inputString = myArr.join('');
+
+    // reversing array
+    let reversedArr = myArr.reverse();
+
+    // converting reversed array to string
+    let reversedString = reversedArr.join('');
+
+    // check condition
+    return inputString === reversedString ? true : false;
 };
 
 // Do not edit below this line
