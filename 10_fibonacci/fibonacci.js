aeb9246e756dc8d0ef67d47658eb2bb0f4f9d9a9
dendenjp <daimlerjp@gmail.com>
2022-09-01 03:06:28 -0500

    Added solution to 10_fibonacci using arrays

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let inputNumber = Number(num);
+    let firstNumber = 0;
+    let secondNumber = 1;
 
+    let nextNumber = 0;
+
+    let myFiboArr = [];
+
+    if (inputNumber < 0) return 'OOPS';
+
+    for (let i = 0; i <= inputNumber; i++) {
+        myFiboArr.push(firstNumber);
+
+        nextNumber = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = nextNumber;
+    }
+
+    let lastNumber = myFiboArr[myFiboArr.length - 1];
+
+    return lastNumber;
 };
 
 // Do not edit below this line
