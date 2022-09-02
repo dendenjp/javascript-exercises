73852d9163c0b52f1685ef6e49c830b1ff828c79
dendenjp <daimlerjp@gmail.com>
2022-09-02 01:08:19 -0500

    Added solution to 12_findTheOldest(all test passed)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,24 @@
 const findTheOldest = function (obj) {
-    const findOldestPerson = obj.sort((previousGuy, currentGuy) => {
+    // copying object. Don't want to mutate.
+    let newObj = obj;
+
+    // adding current year to obj with no yearOfDeath
+    newObj.forEach((el) => {
+        if (!el.yearOfDeath) {
+            el.yearOfDeath = new Date().getFullYear();
+        }
+    });
+
+    // sorting by age
+    const oldestPersonSort = newObj.sort((previousGuy, currentGuy) => {
         const prevGuy = previousGuy.yearOfDeath - previousGuy.yearOfBirth;
         const curGuy = currentGuy.yearOfDeath - currentGuy.yearOfBirth;
 
         if (prevGuy > curGuy) return -1;
         else return 1;
     });
-    console.log(findOldestPerson);
-    return findOldestPerson[0];
+    //return the first element of sorted array
+    return oldestPersonSort[0];
 };
 
 // Do not edit below this line
