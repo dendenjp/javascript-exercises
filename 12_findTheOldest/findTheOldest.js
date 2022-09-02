807c3476b94acea8114e4c8ed0e573936061d2ea
dendenjp <daimlerjp@gmail.com>
2022-09-01 23:58:34 -0500

    Added solution but 2nd test failed.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (obj) {
+    const findOldestPerson = obj.sort((previousGuy, currentGuy) => {
+        const prevGuy = previousGuy.yearOfDeath - previousGuy.yearOfBirth;
+        const curGuy = currentGuy.yearOfDeath - currentGuy.yearOfBirth;
 
+        if (prevGuy > curGuy) return -1;
+        else return 1;
+    });
+    console.log(findOldestPerson);
+    return findOldestPerson[0];
 };
 
 // Do not edit below this line
